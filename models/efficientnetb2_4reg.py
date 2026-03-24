a8d135373ad5d97d6ccd9c67a6be1b7f3206c79e
AlbertJojo2313 <jojoalbert35@gmail.com>
2026-03-23 22:12:54 -0400

    Updated version

--- models/efficientnetb2_4reg.py
+++ models/efficientnetb2_4reg.py
@@ -11,7 +11,7 @@ class PokemonClassifierEfficientNetB2_4reg(nn.Module):
         self. model = efficientnet_b2(weights=weights)
 
         in_features = self.model.classifier[1].in_features
-
+        """
         self.model.classifier[1] = nn.Sequential(
             nn.Dropout(0.3),
             nn.Linear(in_features, 1024),
@@ -23,6 +23,19 @@ class PokemonClassifierEfficientNetB2_4reg(nn.Module):
             nn.ReLU(),
             nn.Dropout(0.35),
             nn.Linear(512, num_classes),
+        )
+        """
+        self.model.classifier[1] = nn.Sequential(
+            nn.Dropout(0.3),
+            nn.Linear(in_features, 512),
+            nn.BatchNorm1d(512),
+            nn.ReLU(),
+            nn.Dropout(0.4),
+            nn.Linear(512, 256),
+            nn.BatchNorm1d(256),
+            nn.ReLU(),
+            nn.Dropout(0.35),
+            nn.Linear(256, num_classes),
         )
     # Freeze all layers except the classifier
     def freeze_backbone(self):
